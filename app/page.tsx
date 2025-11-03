86db3a6551b59366592f5268b5ed4aee04915d80
Zahra-Shadman <zahra.shadmani.work@gmail.com>
2025-11-03 17:58:06 +0330

    start to define navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Navbar from "@/components/Navbar/Navbar";
 
 
 export default function app() {
   return (
-    <div className="underline">
-hiiii
+    <div className="">
+
+<Navbar />
     </div>
   );
 }
---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+const Navbar: React.FC = () => {
+  return (
+    <div>
+      navbar
+    </div>
+  );
+};
+
+export default Navbar;
